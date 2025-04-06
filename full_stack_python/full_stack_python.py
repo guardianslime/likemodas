b0129b3564ef38e52a747993d27f22fad43358b4
Kevin <k1navia0@gmail.com>
2025-04-06 16:26:50 -0500

    Initial Commit

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -46,6 +46,10 @@ app = rx.App()
 app.add_page(index)
 app.add_page(pages.about_page, route=navigation.routes.ABOUT_US_ROUTE)
 app.add_page(contact.contact_page, route=navigation.routes.CONTACT_US_ROUTE)
-app.add_page(contact.contact_entries_list_page, route=navigation.routes.CONTACT_ENTRIES_ROUTE)
+app.add_page(
+    contact.contact_entries_list_page, 
+    route=navigation.routes.CONTACT_ENTRIES_ROUTE,
+    on_load=contact.ContactState.list_entries
+)
 app.add_page(pages.pricing_page, route=navigation.routes.PRICING_ROUTE)
 
