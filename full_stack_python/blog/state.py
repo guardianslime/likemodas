7014d3bebcaccfa5d93464e07cfa8f7e9f915d6b
Kevin <k1navia0@gmail.com>
2025-05-05 18:33:29 -0500

    Initial Commit

--- full_stack_python/blog/state.py
+++ full_stack_python/blog/state.py
@@ -7,14 +7,14 @@ from .model import BlogPostModel
 
 class BlogPostState(rx.State):
     posts: List["BlogPostModel"] = []
-    posts: Optional["BlogPostModel"] = None
+    post: Optional["BlogPostModel"] = None
 
     def load_posts(self):
         with rx.session() as session:
             result = session.exec(
                 select(BlogPostModel)
-            )
-            self.posts = result()
+            ).all()
+            self.posts = result
         #return
 
     #def get_post(self):
