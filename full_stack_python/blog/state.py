04f60aa6126e3211b46951d12ddb48bb8d5b5e04
Kevin <k1navia0@gmail.com>
2025-05-14 17:15:41 -0500

    Initial Commit

--- full_stack_python/blog/state.py
+++ full_stack_python/blog/state.py
@@ -17,7 +17,7 @@ class BlogPostState(rx.State):
 
     @rx.var
     def blog_post_id(self):
-        return self.router.page.params.get("blog_id", "")
+        return self.router.page.params.get("blog_id","")
 
     @rx.var
     def blog_post_url(self):
@@ -35,15 +35,15 @@ class BlogPostState(rx.State):
         with rx.session() as session:
             if self.blog_post_id == "":
                 self.post = None
-                return
+                return  
             result = session.exec(
                 select(BlogPostModel).where(
-                    (BlogPostModel.id == self.blog_post_id)
+                    (BlogPostModel.id == self.blog_post_id) 
                 )
             ).one_or_none()
             self.post = result
             if result is None:
-                self.post_content = ""
+                self.post_content =""
                 return
             self.post_content = self.post.content
             self.post_publish_active = self.post.publish_active
@@ -63,7 +63,7 @@ class BlogPostState(rx.State):
             ).all()
             self.posts = result
 
-    def add_post(self, form_data: dict):
+    def add_post(self, form_data:dict):
         with rx.session() as session:
             post = BlogPostModel(**form_data)
             session.add(post)
@@ -71,7 +71,7 @@ class BlogPostState(rx.State):
             session.refresh(post)
             self.post = post
 
-    def save_post_edits(self, post_id: int, updated_data: dict):
+    def save_post_edits(self, post_id:int, updated_data:dict):
         with rx.session() as session:
             post = session.exec(
                 select(BlogPostModel).where(
@@ -85,11 +85,7 @@ class BlogPostState(rx.State):
             session.add(post)
             session.commit()
             session.refresh(post)
-            self.post = session.exec(  # Fetch updated post!
-                select(BlogPostModel).where(
-                    BlogPostModel.id == post_id
-                )
-            ).one_or_none()
+            self.post = post
 
     def to_blog_post(self, edit_page=False):
         if not self.post:
@@ -97,6 +93,7 @@ class BlogPostState(rx.State):
         if edit_page:
             return rx.redirect(f"{self.blog_post_edit_url}")
         return rx.redirect(f"{self.blog_post_url}")
+
 
 class BlogAddPostFormState(BlogPostState):
     form_data: dict = {}
@@ -106,34 +103,40 @@ class BlogAddPostFormState(BlogPostState):
         self.add_post(form_data)
         return self.to_blog_post(edit_page=True)
 
+
 class BlogEditFormState(BlogPostState):
     form_data: dict = {}
 
     @rx.var
     def publish_display_date(self) -> str:
-        if not self.post or not self.post.publish_date:
+        if not self.post:
+            return datetime.now().strftime("%Y-%m-%d")
+        if not self.post.publish_date:
             return datetime.now().strftime("%Y-%m-%d")
         return self.post.publish_date.strftime("%Y-%m-%d")
-
+    
     @rx.var
     def publish_display_time(self) -> str:
-        if not self.post or not self.post.publish_date:
+        if not self.post:
             return datetime.now().strftime("%H:%M:%S")
+        if not self.post.publish_date:
+            return datetime.now().strftime("%H:%M-%S")
         return self.post.publish_date.strftime("%H:%M:%S")
 
     def handle_submit(self, form_data):
         self.form_data = form_data
         post_id = form_data.pop('post_id')
         publish_date = None
-        if 'publish_date' in form_data:
+        if'publish_date' in form_data:
             publish_date = form_data.pop('publish_date')
         publish_time = None
-        if 'publish_time' in form_data:
+        if'publish_time' in form_data:
             publish_time = form_data.pop('publish_time')
+        print(publish_date, publish_time)
+        publish_input_string = f"{publish_date} {publish_time}"
         try:
-            final_publish_date = datetime.strptime(
-                f"{publish_date} {publish_time}", "%Y-%m-%d %H:%M:%S")
-        except (ValueError, TypeError):  # Catch both errors
+            final_publish_date = datetime.strptime(publish_input_string, "%Y-%m-%d %H:%M:%S")
+        except:
             final_publish_date = None
         publish_active = False
         if 'publish_active' in form_data:
