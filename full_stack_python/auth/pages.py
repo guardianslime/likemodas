846cdbf5a530db2732b5640ac81dc24d331d538d
Kevin <k1navia0@gmail.com>
2025-05-24 20:01:02 -0500

    Initial Commit

--- full_stack_python/auth/pages.py
+++ full_stack_python/auth/pages.py
@@ -9,7 +9,7 @@ def my_login_page()->rx.Component:
     return base_page(
         rx.center(
             rx.cond(
-                loginState.is_hydrated, # type: ignore
+                LoginState.is_hydrated, # type: ignore
                 rx.card(login_form()),
             ),
              min_height="85vh",
