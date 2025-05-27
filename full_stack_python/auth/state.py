18cfc85b2dc856f4763de37f103420b1c27023b1
Kevin <k1navia0@gmail.com>
2025-05-26 22:16:26 -0500

    Initial Commit

--- full_stack_python/auth/state.py
+++ full_stack_python/auth/state.py
@@ -19,10 +19,11 @@ class SessionState(reflex_local_auth.LocalAuthState):
             ).one_or_none()
 
     def on_load(self):
-        if not self.is_authenticated:
-            return reflex_local_auth.LoginState.redir
         print(self.is_authenticated)
         print(self.authenticated_user_info)
+        if not self.is_authenticated:
+            return reflex_local_auth.LoginState.redir
+        
 
 class MyRegisterState(reflex_local_auth.RegistrationState):
     def handle_registration(self, form_data
