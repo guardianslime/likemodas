3a02f7e6083c5e723d8b109ca8b494fbc6732428
Kevin <k1navia0@gmail.com>
2025-05-29 15:40:27 -0500

    Initial Commit

--- full_stack_python/ui/base.py
+++ full_stack_python/ui/base.py
@@ -1,20 +1,12 @@
 import reflex as rx
 
 from .nav import navbar
-from .sidebar import sidebar
+from .dashboard import base_dashboard_page
 
-def base_page(child: rx.Component, hide_navbar=False, *args, **kwargs) -> rx.Component:
-    # print(type(x) for x in args)
-    if not isinstance(child, rx.Component):
-        child = rx.heading("This is not valid child element")
-    if hide_navbar:
-        return rx.container(
-            child,
-            rx.logo(),
-            rx.color_mode.button(oosittion= "bottom-left"),
-    )
+def base_layout_component(child, *args, **Kwargs) -> rx.Component:
+
     return rx.fragment(
-        sidebar(),
+        navbar(),
         rx.box(
             child,
             #bg=rx.color("accent", 3),
@@ -25,5 +17,15 @@ def base_page(child: rx.Component, hide_navbar=False, *args, **kwargs) -> rx.Com
         rx.logo(),
         rx.color_mode.button(position= "bottom-left"),
         id="my-base-container",
+    )
+
+def base_page(child: rx.Component, *args, **kwargs) -> rx.Component:
+    is_logged_in = True
+    if not isinstance(child, rx.Component):
+        child = rx.heading("This is not valid child element")
+    return rx.cond(
+        is_logged_in,
+        base_dashboard_page(child, *args, **kwargs),
+        base_layout_component(child, *args, **kwargs),
     )
 
