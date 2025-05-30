d90a9aeaeac77ed9d575a91ffdaad96a69e57962
Kevin <k1navia0@gmail.com>
2025-05-30 18:36:34 -0500

    Initial Commit

--- full_stack_python/auth/pages.py
+++ full_stack_python/auth/pages.py
@@ -41,8 +41,11 @@ def my_logout_page() -> rx.Component:
     # Welcome Page (Index)
         my_child=rx.vstack(
             rx.heading("Are you sure you want logout?", size="7"),
-
-            rx.button("No", color_scheme="grey"),            
+            rx.link(
+                 rx.button("No", color_scheme="gray"),
+                 href=navigation.routes.HOME_ROUTE
+            ),
+            rx.button("No", color_scheme="gray"),            
             rx.button("Yes, please logout", on_click=SessionState.perform_logout),
             spacing="5",
             justify="center",
