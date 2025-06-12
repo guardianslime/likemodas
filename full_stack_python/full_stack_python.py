a865f76b6c24dfc54352344bf5da5b346b2a4018
Kevin <k1navia0@gmail.com>
2025-06-11 22:25:24 -0500

    Initial Commit

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -97,7 +97,7 @@ app.add_page(
 
 app.add_page(
      article_detail_page,
-     route=f"{navigation.routes.ARTICLE_LIST_ROUTE}/[article_id]",
+     route=f"{navigation.routes.ARTICLE_LIST_ROUTE}/[post_id]",
      on_load=ArticlePublicState.get_post_detail
 )
 
