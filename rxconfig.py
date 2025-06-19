6bf4c0a5105cedd818a6362397f2ad2cebf28126
Kevin <k1navia0@gmail.com>
2025-06-19 16:31:27 -0500

    Initial Commit

--- rxconfig.py
+++ rxconfig.py
@@ -5,13 +5,13 @@ from typing import List
 class FullStackPythonConfig(rx.Config):
     app_name = "full_stack_python"
     # URL de tu backend en Render. Reflex usará HTTPS para API y WSS para WebSockets si esta es HTTPS.
-    #api_url = "https://page-lb9g.onrender.com" 
+    api_url = "https://page-production-8756.up.railway.app/" 
 
     cors_allowed_origins: List[str] = [
         "http://localhost:3000", # Para desarrollo local
         # ¡IMPORTANTE! Asegúrate de que esta URL de Vercel sea la EXACTA.
         # Le quitamos la barra final para consistencia.
-        "https://frontedpage-t5mu.vercel.app/", # URL de tu frontend en Vercel
+        "https://fronted-kohl.vercel.app/", # URL de tu frontend en Vercel
         "http://localhost:8000" # A veces necesario para el backend local
     ]
 
