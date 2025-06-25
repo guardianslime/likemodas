78df91554e2fe4875406ab1e69cf8600bd8a9eda
guardianslime <k1navia0@gmail.com>
2025-06-25 04:29:36 -0500

    Initial Commit

--- full_stack_python/contact/detail.py
+++ full_stack_python/contact/detail.py
@@ -4,7 +4,7 @@ from ..ui.base import base_page
 
 from . import state
 from .notfound import contact_post_not_found
-
+# @rx.page(route="/about")
 def contact_post_detail_page() -> rx.Component:
     con_edit = True
     edit_link = rx.link("Edit", href=f"{state.ContactPostState.contact_post_edit_url}")
@@ -18,22 +18,13 @@ def contact_post_detail_page() -> rx.Component:
             edit_link_el,
             align='end'
         ),
-        # --- CORRECCIÓN ---
-        # Se muestran atributos específicos como .email y .username
-        # en lugar de usar el método inexistente .to_string()
-        rx.text("Author Email: ", state.ContactPostState.post.userinfo.email),
-        rx.text("Author Username: ", state.ContactPostState.post.userinfo.user.username),
-        rx.text("Published on: ", state.ContactPostState.post.publish_date),
-        rx.box(
-            rx.text(
-                state.ContactPostState.post.content,
-                white_space='pre-wrap'
-            ),
-            border="1px solid #ddd",
-            padding="1em",
-            border_radius="8px",
-            margin_top="1em",
-            width="100%"
+        rx.text("User info id", state.ContactPostState.post.userinfo_id),
+        rx.text("User info: ", state.ContactPostState.post.userinfo.to_string()),
+        rx.text("User: ", state.ContactPostState.post.userinfo.user.to_string()),
+        rx.text(state.ContactPostState.post.publish_date),
+        rx.text(
+            state.ContactPostState.post.content,
+            white_space='pre-wrap'
         ),
         spacing="5",
         align="center",
