5a7a8f131669a136051370320203b4c841cab05c
guardianslime <k1navia0@gmail.com>
2025-06-25 17:34:07 -0500

    Initial Commit

--- full_stack_python/contact/notfound.py
+++ full_stack_python/contact/notfound.py
@@ -1,6 +1,6 @@
 import reflex as rx
 
-def contact_post_not_found() -> rx.Component:
+def contact_entry_not_found() -> rx.Component:
     return rx.hstack(
         rx.heading("Contact EntryNot Found"), spacing="5",
         align="center",
