84700d990846fe0172861c6dd36f74bb272a163c
guardianslime <k1navia0@gmail.com>
2025-06-25 18:25:54 -0500

    Initial Commit

--- full_stack_python/contact/detail.py
+++ full_stack_python/contact/detail.py
@@ -7,23 +7,23 @@ from .notfound import contact_entry_not_found
 # @rx.page(route="/about")
 def contact_entry_detail_page() -> rx.Component:
     con_edit = True
-    edit_link = rx.link("Edit", href=f"{state.ContactEntrytState.contact_entry_edit_url}")
+    edit_link = rx.link("Edit", href=f"{state.ContactEntryState.contact_entry_edit_url}")
     edit_link_el = rx.cond(
         con_edit,
         edit_link,
     )
-    my_child = rx.cond(state.ContactEntrytState.entry, rx.vstack(
+    my_child = rx.cond(state.ContactEntryState.entry, rx.vstack(
         rx.hstack(
-            rx.heading(state.ContactEntrytState.entry.title, size="9"),
+            rx.heading(state.ContactEntryState.entry.title, size="9"),
             edit_link_el,
             align='end'
         ),
-        rx.text("User info id", state.ContactEntrytState.entry.userinfo_id),
-        rx.text("User info: ", state.ContactEntrytState.entry.userinfo.to_string()),
-        rx.text("User: ", state.ContactEntrytState.entry.userinfo.user.to_string()),
-        rx.text(state.ContactEntrytState.entry.publish_date),
+        rx.text("User info id", state.ContactEntryState.entry.userinfo_id),
+        rx.text("User info: ", state.ContactEntryState.entry.userinfo.to_string()),
+        rx.text("User: ", state.ContactEntryState.entry.userinfo.user.to_string()),
+        rx.text(state.ContactEntryState.entry.publish_date),
         rx.text(
-            state.ContactEntrytState.entry.content,
+            state.ContactEntryState.entry.content,
             white_space='pre-wrap'
         ),
         spacing="5",
