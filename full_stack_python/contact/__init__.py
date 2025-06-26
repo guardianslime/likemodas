55bd9ce4467c73c512ce53261cc5ee01db61387a
guardianslime <k1navia0@gmail.com>
2025-06-26 16:41:28 -0500

    Initial Commit

--- full_stack_python/contact/__init__.py
+++ full_stack_python/contact/__init__.py
@@ -1,13 +1,10 @@
-from .add import contact_entry_add_page
-from .detail import contact_entry_detail_page
-from .edit import contact_entry_edit_page
-from .list import contact_entry_list_page
-from .state import ContactEntryState
+from .form import contact_form
+from .state import ContactState
+from .page import contact_page, contact_entries_list_page
 
 __all__ = [
-    'contact_entry_add_page',
-    'contact_entry_detail_page',
-    'contact_entry_edit_page',
-    'contact_entry_list_page',
-    'ContactEntryState'
+    "contact_form",
+    "ContactState", 
+    "contact_page",
+    "contact_entries_list_page"
 ]
