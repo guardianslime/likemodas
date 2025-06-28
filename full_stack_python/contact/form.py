ad8cd3814440b9c22db852a1503dc65d4862c565
guardianslime <k1navia0@gmail.com>
2025-06-28 02:51:41 -0500

    Initial Commit

--- full_stack_python/contact/form.py
+++ full_stack_python/contact/form.py
@@ -6,18 +6,18 @@ from .state import ContactState # Apunta al estado simple y único
 def contact_form() -> rx.Component:
     """El formulario para crear una nueva entrada de contacto."""
     my_form = rx.form(
-            rx.vstack(
-                rx.hstack(
-                    rx.input(name="first_name", placeholder="Nombre", width="100%"),
-                    rx.input(name="last_name", placeholder="Apellido", width="100%"),
-                    width="100%",
-                ),
-                rx.input(name="email", placeholder="Tu Email", type="email", width="100%"), 
-                rx.text_area(name="message", placeholder="Tu mensaje", required=True, width='100%'),
-                rx.button("Enviar", type="submit"),
+        rx.vstack(
+            rx.hstack(
+                rx.input(name="first_name", placeholder="Nombre", width="100%"),
+                rx.input(name="last_name", placeholder="Apellido", width="100%"),
+                width="100%",
             ),
-            on_submit=ContactState.handle_submit, # Usa el manejador del estado único
-            reset_on_submit=True,
+            rx.input(name="email", placeholder="Tu Email", type="email", width="100%"), 
+            rx.text_area(name="message", placeholder="Tu mensaje", required=True, width='100%'),
+            rx.button("Enviar", type="submit"),
+        ),
+        on_submit=ContactState.handle_submit, # Usa el manejador del estado único
+        reset_on_submit=True,
     )
 
     my_child = rx.vstack(
@@ -32,14 +32,14 @@ def contact_form() -> rx.Component:
             rx.tablet_only(
                 rx.box(
                     my_form,
-                    width="30vw",
+                    width="50vw",
                 )
             ),
             rx.mobile_only(
                 rx.box(
                     my_form,
                     id= "my-form-box",
-                    width="20vw",
+                    width="50vw",
                 )
             ),
             spacing="5",
@@ -48,5 +48,5 @@ def contact_form() -> rx.Component:
             min_height="85vh",
             id='my-child'
         )
-
+    
     return rx.form(my_child)
