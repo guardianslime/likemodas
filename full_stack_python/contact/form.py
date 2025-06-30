308fc34249f7a5071d435605d148e2fde53f40c9
guardianslime <k1navia0@gmail.com>
2025-06-30 13:52:34 -0500

    Initial Commit

--- full_stack_python/contact/form.py
+++ full_stack_python/contact/form.py
@@ -5,7 +5,7 @@ from .state import ContactState # Apunta al estado simple y único
 
 def contact_form() -> rx.Component:
     """El formulario para crear una nueva entrada de contacto."""
-    my_form = rx.form(
+    return rx.form(
         rx.vstack(
             rx.hstack(
                 rx.input(name="first_name", placeholder="Nombre", width="100%"),
@@ -19,34 +19,3 @@ def contact_form() -> rx.Component:
         on_submit=ContactState.handle_submit, # Usa el manejador del estado único
         reset_on_submit=True,
     )
-
-    my_child = rx.vstack(
-            rx.heading("Contact us", size="9"),
-            rx.cond(ContactState.did_submit, rx.text("Submitted"), rx.button("Default")),
-            rx.desktop_only(
-                rx.box(
-                    my_form,
-                    width="50vw",
-                )
-            ),
-            rx.tablet_only(
-                rx.box(
-                    my_form,
-                    width="50vw",
-                )
-            ),
-            rx.mobile_only(
-                rx.box(
-                    my_form,
-                    id= "my-form-box",
-                    width="50vw",
-                )
-            ),
-            spacing="5",
-            justify="center",
-            align="center",
-            min_height="85vh",
-            id='my-child'
-        )
-    
-    return rx.form(my_child)
