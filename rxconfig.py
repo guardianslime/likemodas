8501329f9ce85e73e428f631708e41792bdbe75e
guardianslime <k1navia0@gmail.com>
2025-06-30 16:49:43 -0500

    Initial Commit

--- rxconfig.py
+++ rxconfig.py
@@ -8,7 +8,7 @@ BACKEND_PRODUCTION_URL = "web-production-50b7a.up.railway.app"
 # La URL donde vivirá tu frontend (Vercel)
 # EN TU ARCHIVO ACTUALMENTE
 # CÓDIGO CORREGIDO
-FRONTEND_PRODUCTION_URL = "https://full-stack-python-ywf4.vercel.app"  # ¡Asegúrate que esta sea tu URL de Vercel!
+FRONTEND_PRODUCTION_URL = "https://full-stack-python.vercel.app"  # ¡Asegúrate que esta sea tu URL de Vercel!
 
 class FullStackPythonConfig(rx.Config):
     app_name = "full_stack_python"
