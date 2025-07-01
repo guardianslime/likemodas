2ffb65294299c920a7576aa6fce0de08ed1fc609
guardianslime <k1navia0@gmail.com>
2025-07-01 16:09:59 -0500

    Initial Commit

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\k1nav\Music\page\full-stack-python\.venv"
+set "VIRTUAL_ENV=C:\dev\page\full-stack-python\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
