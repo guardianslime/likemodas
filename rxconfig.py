d291ece98e8719dee9427125f80fa820060e767a
guardianslime <k1navia0@gmail.com>
2025-07-01 18:26:23 -0500

    Initial Commit

--- rxconfig.py
+++ rxconfig.py
@@ -5,4 +5,12 @@ config = rx.Config(
     db_url="postgresql://postgres:zLkrpBKVsoWqPMpYNyOatYpZZCeoKigK@nozomi.proxy.rlwy.net:46298/railway",
     api_url="https://full-stack-python-production.up.railway.app",
     deploy_url="https://full-stack-python.vercel.app",
+    cors_allowed_origins=[
+        "https://full-stack-python.vercel.app"
+    ]
 )
+
+cors_allowed_origins=[
+    "https://full-stack-python.vercel.app",
+    "http://localhost:3000"
+]
