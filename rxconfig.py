7c2fa3a29f89c65af6d632bfc5c0d49e4536f8d8
guardianslime <k1navia0@gmail.com>
2025-07-01 18:18:08 -0500

    Initial Commit

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="full_stack_python",
-    db_url="postgresql://postgres:zLkrpBKVsoWqPMpYNyOatYpZZCeoKigK@postgres.railway.internal:5432/railway",
+    db_url="postgresql://postgres:zLkrpBKVsoWqPMpYNyOatYpZZCeoKigK@nozomi.proxy.rlwy.net:46298/railway",
     api_url="https://full-stack-python-production.up.railway.app",
     deploy_url="https://full-stack-python.vercel.app",
 )
