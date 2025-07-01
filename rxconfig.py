4c555281339e10261cd8047104006f3d88d3338b
guardianslime <k1navia0@gmail.com>
2025-07-01 18:39:19 -0500

    Initial Commit

--- rxconfig.py
+++ rxconfig.py
@@ -2,11 +2,12 @@ import reflex as rx
 
 config = rx.Config(
     app_name="full_stack_python",
-    db_url="postgresql://postgres:zLkrpBKVsoWqPMpYNyOatYpZZCeoKigK@nozomi.proxy.rlwy.net:46298/railway",
+    db_url="postgresql://postgres:...@nozomi.proxy.rlwy.net:46298/railway",
     api_url="https://full-stack-python-production.up.railway.app",
     deploy_url="https://full-stack-python.vercel.app",
     cors_allowed_origins=[
-        "https://full-stack-python.vercel.app"
+        "https://full-stack-python.vercel.app",
+        "http://localhost:3000"
     ]
 )
 
