594a5728ebdf327ff239fc3d4601b5b678215642
guardianslime <k1navia0@gmail.com>
2025-07-04 16:41:39 -0500

    Initial Commit

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+!/bin/bash
+
+# 1. Instala las dependencias (tu comando original)
 pip install -r requirements.txt
+
+# 2. (¡NUEVO!) Aplica las migraciones a la base de datos
+# Esto sincronizará tu base de datos en Railway con tus modelos de Python
+reflex db migrate
+
+# 3. Inicia el backend de la aplicación (tu comando original)
 reflex run --backend-only --env prod
