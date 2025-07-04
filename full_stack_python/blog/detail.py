3949fecae15c671576216573e937b5621b4c240c
guardianslime <k1navia0@gmail.com>
2025-07-04 17:30:28 -0500

    Initial Commit

--- full_stack_python/blog/detail.py
+++ full_stack_python/blog/detail.py
@@ -7,6 +7,7 @@ from .notfound import blog_post_not_found
 
 def blog_post_detail_page() -> rx.Component:
     edit_link = rx.link("Editar", href=state.BlogPostState.blog_post_edit_url)
+    
     my_child = rx.cond(
         (state.BlogPostState.post) & 
         (state.BlogPostState.post.userinfo) & 
@@ -20,6 +21,7 @@ def blog_post_detail_page() -> rx.Component:
                 justify='between',
                 width="100%"
             ),
+            
             rx.text(
                 "Publicado por: ", 
                 rx.code(state.BlogPostState.post.userinfo.user.username),
@@ -27,19 +29,30 @@ def blog_post_detail_page() -> rx.Component:
                 rx.code(state.BlogPostState.post.userinfo.email),
                 ")"
             ),
+            
             rx.cond(
                 state.BlogPostState.post.publish_date,
                 rx.text(
                     "Fecha de publicación: ",
+                    # Usamos .to_string() para mostrar la fecha de forma segura
                     state.BlogPostState.post.publish_date.to_string()
                 ),
                 rx.text("Este post aún no ha sido publicado.")
             ),
+
             rx.divider(width="100%"),
+            
+            # --- ¡CORRECCIÓN AQUÍ! ---
+            # Usamos la nueva variable 'image_preview_url'
             rx.cond(
-                state.BlogPostState.image_url,
-                rx.image(src=state.BlogPostState.image_url, width="100%", max_width="600px")
+                state.BlogPostState.image_preview_url != "",
+                rx.image(
+                    src=state.BlogPostState.image_preview_url, 
+                    width="100%", 
+                    max_width="600px"
+                )
             ),
+            
             rx.text(
                 state.BlogPostState.post.content,
                 white_space='pre-wrap'
