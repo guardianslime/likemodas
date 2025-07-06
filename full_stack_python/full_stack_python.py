4fa2c67a2539f5cb95bc7ffe27db31372b4a3d1f
guardianslime <k1navia0@gmail.com>
2025-07-06 00:34:21 -0500

    Initial Commit

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -1,18 +1,9 @@
 import reflex as rx
-import os
 
 class State(rx.State):
     uploaded_files: list[str] = []
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Inicializar la lista con los archivos ya guardados en el volumen
-        upload_dir = rx.get_upload_dir()
-        if upload_dir.exists():
-            self.uploaded_files = [
-                f.name for f in upload_dir.iterdir() if f.is_file()
-            ]
-
+    @rx.event
     async def handle_upload(self, files: list[rx.UploadFile]):
         for file in files:
             data = await file.read()
