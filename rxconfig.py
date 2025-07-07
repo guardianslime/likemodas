7b2bb862b329468ea972a2d59ad5c6b2ff811e1d
guardianslime <k1navia0@gmail.com>
2025-07-07 18:07:00 -0500

    Initial Commit

--- rxconfig.py
+++ rxconfig.py
@@ -8,7 +8,7 @@ cors_origenes = os.getenv("CORS_ALLOWED_ORIGINS", "https://full-stack-python.ver
 
 config = rx.Config(
     app_name="full_stack_python",
-    db_url="postgresql://postgres:zLkrpBKVsoWqPMpYNyOatYpZZCeoKigK@nozomi.proxy.rlwy.net:46298/railway",
+    db_url="postgresql://postgres:rszvQoEjlvQijlSTROgqCEDPiNdQqqmU@nozomi.proxy.rlwy.net:37918/railway",
     api_url="https://full-stack-python-production.up.railway.app",
     deploy_url="https://full-stack-python.vercel.app",
     cors_allowed_origins=[origen.strip() for origen in cors_origenes]
