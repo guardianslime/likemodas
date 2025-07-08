6b31e929940974f4695a13f198ca73647edf3371
guardianslime <k1navia0@gmail.com>
2025-07-07 20:05:19 -0500

    Initial Commit

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -1,6 +1,3 @@
-#full_stack_python/full_stack_python.py
-
-
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
@@ -11,17 +8,18 @@ from rxconfig import config
 from .ui.base import base_page
 from .auth.pages import my_login_page, my_register_page, my_logout_page
 from .auth.state import SessionState
-from .articles.detail import article_detail_page
-from .articles.list import article_public_list_page
-from .articles.state import ArticlePublicState
 
 # Se importan los paquetes completos para usar la notación paquete.componente
-from . import blog, contact, navigation, pages
+from . import contact, navigation, pages
+
+# --- NUEVO: IMPORTAR SOLO EL MÓDULO DE PÁGINAS DEL BLOG ---
+from .blog import page as blog_pages
+
 
 # --- Definición de la Aplicación ---
-
 def index() -> rx.Component:
     """La página principal que redirige al dashboard si el usuario está autenticado."""
+    # Esta parte se puede quedar igual si quieres mantener la autenticación para otras partes.
     return base_page(
         rx.cond(
             SessionState.is_authenticated,
@@ -43,7 +41,7 @@ app = rx.App(
 
 # --- Registro de Páginas ---
 
-app.add_page(index, on_load=ArticlePublicState.load_posts)
+app.add_page(index)
 app.add_page(my_login_page, route=reflex_local_auth.routes.LOGIN_ROUTE)
 app.add_page(my_register_page, route=reflex_local_auth.routes.REGISTER_ROUTE)
 app.add_page(my_logout_page, route=navigation.routes.LOGOUT_ROUTE)
@@ -51,38 +49,12 @@ app.add_page(pages.about_page, route=navigation.routes.ABOUT_US_ROUTE)
 app.add_page(pages.protected_page, route="/protected/", on_load=SessionState.on_load)
 app.add_page(pages.pricing_page, route=navigation.routes.PRICING_ROUTE)
 
-# Páginas de Artículos
-app.add_page(
-    article_public_list_page,
-    route=navigation.routes.ARTICLE_LIST_ROUTE,
-    on_load=ArticlePublicState.load_posts,
-)
-app.add_page(
-    article_detail_page,
-    route=f"{navigation.routes.ARTICLE_LIST_ROUTE}/[article_id]",
-    on_load=ArticlePublicState.get_post_detail,
-)
+# --- RUTAS DEL BLOG SIMPLIFICADAS ---
+# (Elimina todas las rutas anteriores del blog)
+app.add_page(blog_pages.blog_list_page, route="/blog")
+app.add_page(blog_pages.blog_add_page, route="/blog/add")
 
-# Páginas de Blog
-app.add_page(
-    blog.blog_post_list_page,
-    route=navigation.routes.BLOG_POSTS_ROUTE,
-    on_load=blog.BlogPostState.load_posts
-)
-app.add_page(blog.blog_post_add_page, route=navigation.routes.BLOG_POST_ADD_ROUTE)
-app.add_page(
-    blog.blog_post_detail_page,
-    route="/blog/[blog_id]",
-    on_load=blog.BlogPostState.get_post_detail
-)
-app.add_page(
-    blog.blog_post_edit_page,
-    route="/blog/[blog_id]/edit",
-    on_load=blog.BlogPostState.get_post_detail
-)
-
-
-# Páginas de Contacto
+# Páginas de Contacto (las puedes dejar si las usas)
 app.add_page(contact.contact_page, route=navigation.routes.CONTACT_US_ROUTE)
 app.add_page(
     contact.contact_entries_list_page,
