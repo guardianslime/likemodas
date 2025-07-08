d5691f91288326907da25709ffb5045b9ece655a
guardianslime <k1navia0@gmail.com>
2025-07-07 23:57:16 -0500

    InitiaL Commit

--- full_stack_python/blog/state.py
+++ full_stack_python/blog/state.py
@@ -144,7 +144,7 @@ class BlogViewState(rx.State):
 
     @rx.var
     def post_id(self) -> str:
-        return self.router.page.params.get("id", "")
+        return self.router.page.params.get("post_id", "")
 
     def on_load(self):
         try:
