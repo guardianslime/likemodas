96438da9ee5131d57520a5a387245f844307e1ac
guardianslime <k1navia0@gmail.com>
2025-07-08 00:25:12 -0500

    Initial Commit

--- full_stack_python/blog/page.py
+++ full_stack_python/blog/page.py
@@ -9,7 +9,7 @@ def blog_public_page():
             lambda post: rx.box(
                 rx.link(
                     rx.cond(
-                        post.images and len(post.images) > 0,
+                        (post.images != None) & (post.images.length() > 0),
                         rx.image(src=rx.get_upload_url(post.images[0]), width="200px"),
                         rx.box("Sin imagen", width="200px", height="150px", bg="#eee", align="center", justify="center")
                     ),
