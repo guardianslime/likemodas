758d5f7b540fc182e79959394ba5dd82e447dc4d
guardianslime <k1navia0@gmail.com>
2025-07-09 02:39:26 -0500

    Initial Commit

--- full_stack_python/blog/page.py
+++ full_stack_python/blog/page.py
@@ -213,7 +213,7 @@ def blog_public_page():
                             ),
                             columns="repeat(6, 1fr)",
                             spacing="4",
-                            width="100%",
+                            width="200%",
                             max_width="1400px",  # ⬅️ más ancho para que ocupen más pantalla
                             justify_content="center"
                         )
