bf0d62de29ebeb0e90d84a931ac00caf0b88f54e
guardianslime <k1navia0@gmail.com>
2025-07-10 03:48:53 -0500

    Initial Commit

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -6,10 +6,9 @@
 import reflex as rx
 import reflex_local_auth
 from full_stack_python.blog.forms import blog_post_add_form
-from full_stack_python.public_post.state import blog_public_page
+from full_stack_python.public_post.view import blog_public_page
 from full_stack_python.public_post.state import BlogViewState
-from full_stack_python.public_post.state import blog_post_view_page
-from full_stack_python.public_post.state import public_post_detail_page
+from full_stack_python.public_post.view import blog_post_view_page, public_post_detail_page
 from rxconfig import config
 
 # --- Módulos y Componentes ---
