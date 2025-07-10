19fc87892748936677de24aed94f08067906e61a
guardianslime <k1navia0@gmail.com>
2025-07-10 16:04:04 -0500

    Initial Commit

--- full_stack_python/public_post/view.py
+++ full_stack_python/public_post/view.py
@@ -3,37 +3,92 @@ from full_stack_python.public_post.state import BlogViewState
 from full_stack_python.ui.base import base_layout_component
 
 def public_post_detail_page():
+    # Vista común que se adapta a tamaños
+    def content():
+        return rx.hstack(
+            # Imagen + flechas
+            rx.box(
+                rx.box(
+                    rx.image(
+                        src=rx.cond(
+                            BlogViewState.imagen_actual != "",
+                            rx.get_upload_url(BlogViewState.imagen_actual),
+                            "/no_image.png"
+                        ),
+                        width="100%",
+                        height="100%",
+                        object_fit="contain",
+                        border_radius="md"
+                    ),
+                    width="100%",
+                    height="100%",
+                    position="relative"
+                ),
+                rx.icon(
+                    tag="arrow_big_left",
+                    position="absolute",
+                    left="0.5em",
+                    top="50%",
+                    transform="translateY(-50%)",
+                    on_click=BlogViewState.anterior_imagen,
+                    cursor="pointer",
+                    box_size="2em"
+                ),
+                rx.icon(
+                    tag="arrow_big_right",
+                    position="absolute",
+                    right="0.5em",
+                    top="50%",
+                    transform="translateY(-50%)",
+                    on_click=BlogViewState.siguiente_imagen,
+                    cursor="pointer",
+                    box_size="2em"
+                ),
+                width="100%",
+                max_width="600px",
+                height="400px",
+                position="relative",
+                border_radius="md",
+                overflow="hidden",
+            ),
+            # Info
+            rx.box(
+                rx.vstack(
+                    rx.text(
+                        BlogViewState.post.title,
+                        size="6",
+                        font_weight="bold",
+                        margin_bottom="0.5em"
+                    ),
+                    rx.text(
+                        BlogViewState.formatted_price,
+                        size="5",
+                        color="gray"
+                    ),
+                    rx.text(
+                        BlogViewState.content,
+                        size="4",
+                        margin_top="1em",
+                        white_space="pre-wrap"
+                    )
+                ),
+                width="100%",
+                padding="1em"
+            ),
+            spacing="6",
+            width="100%",
+            align_items="start",
+            wrap="wrap"
+        )
+
     return base_layout_component(
         rx.box(
             rx.cond(
                 BlogViewState.has_post,
                 rx.fragment(
-                    rx.desktop_only(
-                        rx.hstack(
-                            _image_section(),
-                            _info_section(),
-                            spacing="6",
-                            width="100%",
-                            align_items="start",
-                            wrap="wrap"
-                        )
-                    ),
-                    rx.tablet_only(
-                        rx.vstack(
-                            _image_section(),
-                            _info_section(),
-                            spacing="4",
-                            width="100%",
-                        )
-                    ),
-                    rx.mobile_only(
-                        rx.vstack(
-                            _image_section(),
-                            _info_section(),
-                            spacing="4",
-                            width="100%",
-                        )
-                    )
+                    rx.mobile_only(content()),
+                    rx.tablet_only(content()),
+                    rx.desktop_only(content()),
                 ),
                 rx.center(
                     rx.text("Publicación no encontrada.", color="red")
@@ -45,75 +100,4 @@ def public_post_detail_page():
             margin="0 auto"
         ),
         on_mount=BlogViewState.on_load
-    )
-
-def _image_section():
-    return rx.box(
-        rx.box(
-            rx.image(
-                src=rx.cond(
-                    BlogViewState.imagen_actual != "",
-                    rx.get_upload_url(BlogViewState.imagen_actual),
-                    "/no_image.png"
-                ),
-                width="100%",
-                height="100%",
-                object_fit="contain",
-                border_radius="md"
-            ),
-            width="100%",
-            height="100%",
-            position="relative"
-        ),
-        rx.icon(
-            tag="arrow_big_left",
-            position="absolute",
-            left="0.5em",
-            top="50%",
-            transform="translateY(-50%)",
-            on_click=BlogViewState.anterior_imagen,
-            cursor="pointer",
-            box_size="2em"
-        ),
-        rx.icon(
-            tag="arrow_big_right",
-            position="absolute",
-            right="0.5em",
-            top="50%",
-            transform="translateY(-50%)",
-            on_click=BlogViewState.siguiente_imagen,
-            cursor="pointer",
-            box_size="2em"
-        ),
-        width="60%",
-        max_width="600px",
-        height="500px",
-        position="relative",
-        border_radius="md",
-        overflow="hidden",
-    )
-
-def _info_section():
-    return rx.box(
-        rx.vstack(
-            rx.text(
-                BlogViewState.post.title,
-                size="6",
-                font_weight="bold",
-                margin_bottom="0.5em"
-            ),
-            rx.text(
-                BlogViewState.formatted_price,
-                size="5",
-                color="gray"
-            ),
-            rx.text(
-                BlogViewState.content,
-                size="4",
-                margin_top="1em",
-                white_space="pre-wrap"
-            )
-        ),
-        width="40%",
-        padding="2em"
     )
