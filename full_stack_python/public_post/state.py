2c127ceab3378ebe35722c3ed159056c1884f0d4
guardianslime <k1navia0@gmail.com>
2025-07-10 04:25:56 -0500

    Initial Commit

--- full_stack_python/public_post/state.py
+++ full_stack_python/public_post/state.py
@@ -8,7 +8,7 @@ class BlogViewState(rx.State):
 
     @rx.var
     def post_id(self) -> str:
-        return self.router.url.params.get("public_post_id", "")
+        return self.router.url.query.get("public_post_id", "")
 
     @rx.var
     def imagen_actual(self) -> str:
