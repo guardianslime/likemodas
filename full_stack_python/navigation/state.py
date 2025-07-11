5a2784bc1b0181b4e73af08262ade7002e0c9d87
guardianslime <k1navia0@gmail.com>
2025-07-11 15:43:02 -0500

    Initial Commit

--- full_stack_python/navigation/state.py
+++ full_stack_python/navigation/state.py
@@ -2,23 +2,6 @@ import reflex as rx
 import reflex_local_auth
 from . import routes
 
-
-class DeviceState(rx.State):
-    is_mobile: bool = False
-    is_desktop: bool = False
-
-    def on_mount(self):
-        return rx.call_script(
-            """
-            if (window.innerWidth < 768) {
-                DeviceState.set_is_mobile(true);
-            } else {
-                DeviceState.set_is_desktop(true);
-            }
-            """
-        )
-
-
 class NavState(rx.State):
     def to_home(self):
         return rx.redirect(routes.HOME_ROUTE)
