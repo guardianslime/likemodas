c503299b0b88e57eb7d355681e586191a2ffb3d5
guardianslime <k1navia0@gmail.com>
2025-07-13 15:27:49 -0500

    Initial Commit

--- full_stack_python/blog/page.py
+++ full_stack_python/blog/page.py
@@ -1,6 +1,6 @@
 import reflex as rx
 # Se importa base_page para usarlo como layout principal
-from ..ui.base import base_page
+from ..ui.base import base_page 
 from full_stack_python.blog.state import BlogPublicState
 from full_stack_python.navigation import routes
 
@@ -15,6 +15,152 @@ def blog_public_page():
 
                 # Mobile (2 columnas)
                 rx.mobile_only(
+                    rx.grid(
+                        rx.foreach(
+                            BlogPublicState.posts,
+                            lambda post: rx.box(
+                                rx.link(
+                                    rx.vstack(
+                                        rx.box(
+                                            rx.cond(
+                                                post.images & (post.images.length() > 0),
+                                                rx.image(
+                                                    src=rx.get_upload_url(post.images[0]),
+                                                    width="100%",
+                                                    height="100%",
+                                                    object_fit="cover",
+                                                    border_radius="md"
+                                                ),
+                                                rx.box(
+                                                    "Sin imagen",
+                                                    width="100%",
+                                                    height="100%",
+                                                    bg="#eee",
+                                                    align="center",
+                                                    justify="center",
+                                                    display="flex",
+                                                    border_radius="md"
+                                                )
+                                            ),
+                                            position="relative",
+                                            width="260px",
+                                            height="260px"
+                                        ),
+                                        rx.text(
+                                            post.title,
+                                            weight="bold",
+                                            size="6",
+                                            padding_left="3px",
+                                            white_space="normal",
+                                            word_break="break-word",
+                                            color=rx.color_mode_cond("black", "white"),
+                                        ),
+                                        rx.text(
+                                            rx.cond(
+                                                post.price,
+                                                "$" + post.price.to(str),
+                                                "$0.00"
+                                            ),
+                                            color=rx.color_mode_cond("black", "white"),
+                                            size="6",
+                                            padding_left="3px",
+                                            white_space="normal",
+                                            word_break="break-word"
+                                        ),
+                                        spacing="2",
+                                        align="start"
+                                    ),
+                                    href=f"{routes.PUBLIC_POST_ROUTE}/{post.id}"
+                                ),
+                                bg=rx.color_mode_cond("#f9f9f9", "#111111"),
+                                border=rx.color_mode_cond("1px solid #e5e5e5", "1px solid #1a1a1a"),
+                                border_radius="8px",
+                                box_shadow="md",
+                                min_height="380px"
+                            )
+                        ),
+                        columns="repeat(2, 1fr)",
+                        spacing="6",
+                        width="100%",
+                        justify_content="center"
+                    )
+                ),
+
+                # Tablet (3 columnas)
+                rx.tablet_only(
+                    rx.grid(
+                        rx.foreach(
+                            BlogPublicState.posts,
+                            lambda post: rx.box(
+                                rx.link(
+                                    rx.vstack(
+                                        rx.box(
+                                            rx.cond(
+                                                post.images & (post.images.length() > 0),
+                                                rx.image(
+                                                    src=rx.get_upload_url(post.images[0]),
+                                                    width="100%",
+                                                    height="100%",
+                                                    object_fit="cover",
+                                                    border_radius="md"
+                                                ),
+                                                rx.box(
+                                                    "Sin imagen",
+                                                    width="100%",
+                                                    height="100%",
+                                                    bg="#eee",
+                                                    align="center",
+                                                    justify="center",
+                                                    display="flex",
+                                                    border_radius="md"
+                                                )
+                                            ),
+                                            position="relative",
+                                            width="260px",
+                                            height="260px"
+                                        ),
+                                        rx.text(
+                                            post.title,
+                                            weight="bold",
+                                            size="6",
+                                            padding_left="3px",
+                                            white_space="normal",
+                                            word_break="break-word",
+                                            color=rx.color_mode_cond("black", "white"),
+                                        ),
+                                        rx.text(
+                                            rx.cond(
+                                                post.price,
+                                                "$" + post.price.to(str),
+                                                "$0.00"
+                                            ),
+                                            color=rx.color_mode_cond("black", "white"),
+                                            size="6",
+                                            padding_left="3px",
+                                            white_space="normal",
+                                            word_break="break-word"
+                                        ),
+                                        spacing="2",
+                                        align="start"
+                                    ),
+                                    href=f"{routes.PUBLIC_POST_ROUTE}/{post.id}"
+                                ),
+                                bg=rx.color_mode_cond("#f9f9f9", "#111111"),
+                                border=rx.color_mode_cond("1px solid #e5e5e5", "1px solid #1a1a1a"),
+                                border_radius="8px",
+                                box_shadow="md",
+                                min_height="380px"
+                            )
+                        ),
+                        columns="repeat(3, 1fr)",
+                        spacing="6",
+                        width="100%",
+                        justify_content="center"
+                    )
+                ),
+
+                # Desktop (6 columnas)
+                rx.desktop_only(
                     rx.grid(
                         rx.foreach(
                             BlogPublicState.posts,
@@ -85,50 +231,6 @@ def blog_public_page():
                                 min_height="380px"
                             )
                         ),
-                        columns="repeat(2, 1fr)",
-                        spacing="6",
-                        width="100%",
-                        justify_content="center"
-                    )
-                ),
-
-                # Tablet (3 columnas)
-                rx.tablet_only(
-                    rx.grid(
-                        rx.foreach(
-                            BlogPublicState.posts,
-                            lambda post: rx.box(
-                                rx.link(
-                                    rx.vstack(
-                                        # ... (contenido del vstack idéntico al de mobile)
-                                    ),
-                                    href=f"{routes.PUBLIC_POST_ROUTE}/{post.id}"
-                                ),
-                                # ... (estilos del box idénticos al de mobile)
-                            )
-                        ),
-                        columns="repeat(3, 1fr)",
-                        spacing="6",
-                        width="100%",
-                        justify_content="center"
-                    )
-                ),
-
-                # Desktop (6 columnas)
-                rx.desktop_only(
-                    rx.grid(
-                        rx.foreach(
-                            BlogPublicState.posts,
-                            lambda post: rx.box(
-                                rx.link(
-                                    rx.vstack(
-                                        # ... (contenido del vstack idéntico al de mobile)
-                                    ),
-                                    href=f"{routes.PUBLIC_POST_ROUTE}/{post.id}"
-                                ),
-                                # ... (estilos del box idénticos al de mobile)
-                            )
-                        ),
                         columns="repeat(6, 1fr)",
                         spacing="6",
                         width="100%",
@@ -145,5 +247,5 @@ def blog_public_page():
         width="100%"
     )
     
-    # Se envuelve el contenido con base_page
+    # Se envuelve el contenido con base_page para asegurar la consistencia
     return base_page(my_child)
