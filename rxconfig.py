578b1d57a98bf576b23311c8237b0d8f5c1c12b8
guardianslime <k1navia0@gmail.com>
2025-07-16 01:46:41 -0500

    Initial Commit

--- rxconfig.py
+++ rxconfig.py
@@ -37,6 +37,7 @@ cors_allowed_origins = list(
 
 config = rx.Config(
     app_name="full_stack_python",
+    show_built_with_reflex=False,
     db_url="postgresql://postgres:rszvQoEjlvQijlSTROgqCEDPiNdQqqmU@nozomi.proxy.rlwy.net:37918/railway",
     api_url=API_URL,
     cors_allowed_origins=cors_allowed_origins,
