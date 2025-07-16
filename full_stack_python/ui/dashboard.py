a95a8f043786d9da63ab27ed4ee32e8111b32edf
guardianslime <k1navia0@gmail.com>
2025-07-16 05:06:50 -0500

    Initial Commit

--- full_stack_python/ui/dashboard.py
+++ full_stack_python/ui/dashboard.py
@@ -1,10 +1,9 @@
-# full_stack_python/pages/dashboard.py
 import reflex as rx
-from ..articles.list import componente_de_prueba
+from ..articles.list import articles_gallery_page
 
 def dashboard_component() -> rx.Component:
-    """Un dashboard que solo importa el componente de prueba."""
-    return rx.vstack(
-        rx.heading("Página de Prueba"),
-        componente_de_prueba(),
+    return rx.box(
+        rx.heading("Bienvenido de regreso", size="5", margin_bottom="1em"),
+        rx.divider(),
+        articles_gallery_page(),
     )
