12c587df80ad7fb3ec3adbd0b0063c30002b966f
guardianslime <k1navia0@gmail.com>
2025-07-16 04:50:38 -0500

    Initial Commit

--- full_stack_python/ui/dashboard.py
+++ full_stack_python/ui/dashboard.py
@@ -1,24 +1,26 @@
 import reflex as rx
-
 from .sidebar import sidebar
 
 def base_dashboard_page(child: rx.Component, *args, **kwargs) -> rx.Component:
-    # print(type(x) for x in args)
+    """
+    Define la estructura base para todas las páginas autenticadas,
+    colocando una barra lateral a la izquierda y el contenido principal a la derecha.
+    """
+    # Valida que el elemento hijo sea un componente válido de Reflex.
     if not isinstance(child, rx.Component):
-        child = rx.heading("This is not valid child element")
+        child = rx.heading("This is not a valid child element")
+        
+    # Devuelve un fragmento con la barra lateral y el contenido principal.
     return rx.fragment(
         rx.hstack(
             sidebar(),
             rx.box(
                 child,
-                #bg=rx.color("accent", 3),
                 padding="1em",
                 width="100%",    
                 id="my-content-area-el"
             ),
+            # Asegura que la barra lateral y el contenido estén alineados en la parte superior.
+            align="start" 
         ),
-        # rx.color_mode.button(position= "bottom-left"),
-        # padding="10em",
-        # id="my-base-container",
     )
-
