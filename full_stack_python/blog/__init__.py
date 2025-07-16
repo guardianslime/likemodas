159416831c747fd7492c98516b7f83512e523973
guardianslime <k1navia0@gmail.com>
2025-07-16 11:50:37 -0500

    Initial Commit

--- full_stack_python/blog/__init__.py
+++ full_stack_python/blog/__init__.py
@@ -1,16 +1,19 @@
-# full_stack_python/blog/__init__.py
 from .add import blog_post_add_page
 from .detail import blog_post_detail_page
 from .edit import blog_post_edit_page
 from .list import blog_post_list_page
-from .page import blog_public_page
-from .public_detail import blog_public_detail_page
+from .state import BlogPostState, BlogAddFormState, BlogEditFormState, BlogPublicState, BlogViewState
 
 __all__ = [
     'blog_post_add_page',
     'blog_post_detail_page',
     'blog_post_edit_page',
     'blog_post_list_page',
-    'blog_public_page',
-    'blog_public_detail_page',
+    'BlogPostState',
+    'BlogAddFormState',
+    # --- ✨ CORRECCIÓN AQUÍ ✨ ---
+    # Se añade BlogEditFormState a la lista para que pueda ser importado.
+    'BlogEditFormState',
+    'BlogPublicState',
+    'BlogViewState',
 ]
