7928cab12cddef8ef21ea62eba6e4f28023249a5
guardianslime <k1navia0@gmail.com>
2025-07-17 03:43:04 -0500

    Initial Commit

---
+++ frontend/assets/arrow-big-right-BNbbA6L7.js
@@ -0,0 +1,11 @@
+import{aY as o}from"./createLucideIcon-DVCcLhj4.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M18 15h-6v4l-7-7 7-7v4h6v6z",key:"lbrdak"}]],c=o("arrow-big-left",r);/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M6 9h6V5l7 7-7 7v-4H6V9z",key:"7fvt9c"}]],e=o("arrow-big-right",t);export{c as A,e as a};
