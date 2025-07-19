9c0964dcfda07f93bf2e7518f757c355877068be
guardianslime <k1navia0@gmail.com>
2025-07-19 15:28:43 -0500

    Initial Commit

---
+++ frontend/assets/slice-CR517LHO.js
@@ -0,0 +1,6 @@
+import{bb as o}from"./createLucideIcon-DcOYmfBF.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M11 16.586V19a1 1 0 0 1-1 1H2L18.37 3.63a1 1 0 1 1 3 3l-9.663 9.663a1 1 0 0 1-1.414 0L8 14",key:"1sllp5"}]],i=o("slice",e);export{e as __iconNode,i as default};
