109227788a3094dcf2066f9e9cae8bdb5c697fee
guardianslime <k1navia0@gmail.com>
2025-07-19 11:51:34 -0500

    Initial Commmit

---
+++ frontend/assets/chevrons-left-right-ellipsis-BiCxi_z1.js
@@ -0,0 +1,6 @@
+import{bb as t}from"./createLucideIcon-DcOYmfBF.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M12 12h.01",key:"1mp3jc"}],["path",{d:"M16 12h.01",key:"1l6xoz"}],["path",{d:"m17 7 5 5-5 5",key:"1xlxn0"}],["path",{d:"m7 7-5 5 5 5",key:"19njba"}],["path",{d:"M8 12h.01",key:"czm47f"}]],i=t("chevrons-left-right-ellipsis",e);export{e as __iconNode,i as default};
