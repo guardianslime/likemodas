8f1b6eefe07d3606c7553fa31ac8fa571cd0c1a2
guardianslime <k1navia0@gmail.com>
2025-07-19 21:38:26 -0500

    Initial Commit

---
+++ frontend/assets/swiss-franc-XFnJ8p8N.js
@@ -0,0 +1,6 @@
+import{bb as t}from"./createLucideIcon-BKEtgAre.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M10 21V3h8",key:"br2l0g"}],["path",{d:"M6 16h9",key:"2py0wn"}],["path",{d:"M10 9.5h7",key:"13dmhz"}]],s=t("swiss-franc",o);export{o as __iconNode,s as default};
