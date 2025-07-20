3ca5d229edf12db6c93260f312de3150af6ae082
guardianslime <k1navia0@gmail.com>
2025-07-20 18:55:48 -0500

    Initial Commit

--- likemodas/pages/dashboard.py
+++ likemodas/pages/dashboard.py
@@ -1,11 +1,16 @@
+# likemodas/pages/dashboard.py (VERSIÓN CORREGIDA)
+
 import reflex as rx 
 
-from ..articles.list import article_public_list_component
+# ✨ CAMBIO: Se importa el nuevo componente de galería y el estado del carrito
+from ..blog.page import product_gallery_component
+from ..cart.state import CartState
 
 def dashboard_component() -> rx.Component:
     return rx.box(
         rx.heading("Bienvenido de regreso", size='2'),
-        rx.divider(margin_top='1em', mangin_bottom='1em'),
-        article_public_list_component(columns=3, limit=20),
+        rx.divider(margin_top='1em', margin_bottom='1em'),
+        # ✨ CAMBIO: Se usa el nuevo componente y se le pasa una porción de la lista de posts
+        product_gallery_component(posts=CartState.posts[:20]),
         min_height="85vh",
     )
