90f2e62ed91ac43fa2a9bfeaf0f79221fb0f53e3
guardianslime <k1navia0@gmail.com>
2025-07-19 19:38:14 -0500

    Initial Commit

--- frontend/assets/move-down-BGs2tTMN.js
+++
@@ -1,6 +0,0 @@
-import{bb as o}from"./createLucideIcon-DcOYmfBF.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
- * @license lucide-react v0.511.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=[["path",{d:"M8 18L12 22L16 18",key:"cskvfv"}],["path",{d:"M12 2V22",key:"r89rzk"}]],a=o("move-down",e);export{e as __iconNode,a as default};
