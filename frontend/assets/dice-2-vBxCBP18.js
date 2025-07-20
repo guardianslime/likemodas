31e2cff50a1f5ff4a10e7741c10f0859b94469ef
guardianslime <k1navia0@gmail.com>
2025-07-20 18:46:18 -0500

    Initial Commit

--- frontend/assets/dice-2-vBxCBP18.js
+++
@@ -1,6 +0,0 @@
-import{bb as e}from"./createLucideIcon-BKEtgAre.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
- * @license lucide-react v0.511.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",ry:"2",key:"1m3agn"}],["path",{d:"M15 9h.01",key:"x1ddxp"}],["path",{d:"M9 15h.01",key:"fzyn71"}]],i=e("dice-2",t);export{t as __iconNode,i as default};
