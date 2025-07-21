d49d2803645aec1cb52cc35c319f4f4c0f4929d8
guardianslime <k1navia0@gmail.com>
2025-07-21 01:06:17 -0500

    Initial Commit

--- frontend/assets/tally-3-jwRRsd1L.js
+++
@@ -1,6 +0,0 @@
-import{ba as t}from"./createLucideIcon-DPXTVfDN.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
- * @license lucide-react v0.511.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const a=[["path",{d:"M4 4v16",key:"6qkkli"}],["path",{d:"M9 4v16",key:"81ygyz"}],["path",{d:"M14 4v16",key:"12vmem"}]],y=t("tally-3",a);export{a as __iconNode,y as default};
