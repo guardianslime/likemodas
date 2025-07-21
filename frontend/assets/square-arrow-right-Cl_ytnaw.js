b4909e13f1da69b54f4a1b51ee2ae13f4251f387
guardianslime <k1navia0@gmail.com>
2025-07-21 06:14:40 -0500

    Initial Commit

---
+++ frontend/assets/square-arrow-right-Cl_ytnaw.js
@@ -0,0 +1,6 @@
+import{ba as t}from"./createLucideIcon-DPXTVfDN.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}],["path",{d:"M8 12h8",key:"1wcyev"}],["path",{d:"m12 16 4-4-4-4",key:"1i9zcv"}]],i=t("square-arrow-right",r);export{r as __iconNode,i as default};
