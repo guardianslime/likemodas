6e444e5b5cb561d9ef9c5329518e1734ae22679c
guardianslime <k1navia0@gmail.com>
2025-07-21 01:21:51 -0500

    Initial Commit

---
+++ frontend/assets/rectangle-vertical-gUXwP_8b.js
@@ -0,0 +1,6 @@
+import{ba as t}from"./createLucideIcon-DPXTVfDN.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["rect",{width:"12",height:"20",x:"6",y:"2",rx:"2",key:"1oxtiu"}]],i=t("rectangle-vertical",e);export{e as __iconNode,i as default};
