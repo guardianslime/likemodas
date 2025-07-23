72c56366bd9a6455f0416b2cdd30fc8e9e3092dd
guardianslime <k1navia0@gmail.com>
2025-07-22 23:17:05 -0500

    Initial Commit

--- frontend/assets/square-chevron-left-Jao7X6f8.js
+++
@@ -1,6 +0,0 @@
-import{M as e}from"./createLucideIcon-CVZX51G5.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
- * @license lucide-react v0.511.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}],["path",{d:"m14 16-4-4 4-4",key:"ojs7w8"}]],c=e("square-chevron-left",t);export{t as __iconNode,c as default};
