8986d06beed22ec3e2709104e9bf3aab4e5ed295
guardianslime <k1navia0@gmail.com>
2025-07-23 18:12:57 -0500

    Initial Commit

--- likemodas/pages/__init__.py
+++ likemodas/pages/__init__.py
@@ -6,19 +6,19 @@ from .landing import landing_component
 from .pricing import pricing_page
 from .protected import protected_page
 from . import search_results
-# --- 👇 AÑADE ESTA LÍNEA DE IMPORTACIÓN 👇 ---
-#from . import category_page
+# --- 👇 DESCOMENTA ESTA LÍNEA 👇 ---
+from . import category_page
 from . import test_page
 
 
-__all__ = [ 
+__all__ = [
     'about_page',
     'dashboard_component',
     'landing_component',
     'pricing_page',
     'protected_page',
     'search_results',
-    # --- 👇 Y AÑADE ESTA LÍNEA AQUÍ TAMBIÉN 👇 ---
-    #'category_page'
-    'test_page' 
+    # --- 👇 Y DESCOMENTA ESTA LÍNEA TAMBIÉN 👇 ---
+    'category_page',
+    'test_page'
 ]
