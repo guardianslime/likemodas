bef35aa0c0531ebaaf7b5e293c93cb03b942b8ed
guardianslime <k1navia0@gmail.com>
2025-07-23 01:03:55 -0500

    Initial Commit

--- frontend/assets/copyleft-Cvzf_OPw.js
+++
@@ -1,6 +0,0 @@
-import{M as e}from"./createLucideIcon-D6OVFlI8.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
- * @license lucide-react v0.511.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M9.17 14.83a4 4 0 1 0 0-5.66",key:"1sveal"}]],r=e("copyleft",o);export{o as __iconNode,r as default};
