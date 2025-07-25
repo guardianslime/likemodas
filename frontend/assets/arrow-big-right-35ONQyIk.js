3a70030d0a83b68ba0db32c242bae085cf731189
guardianslime <k1navia0@gmail.com>
2025-07-25 13:24:45 -0500

    Initial Commit

---
+++ frontend/assets/arrow-big-right-35ONQyIk.js
@@ -0,0 +1,6 @@
+import{M as o}from"./createLucideIcon-CVZX51G5.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M6 9h6V5l7 7-7 7v-4H6V9z",key:"7fvt9c"}]],e=o("arrow-big-right",t);export{t as __iconNode,e as default};
