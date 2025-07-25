4ccfc39a5902e5e6a7f9db9c75773cd0bc9e4de8
guardianslime <k1navia0@gmail.com>
2025-07-25 09:56:58 -0500

    Initial Commit

---
+++ frontend/assets/ellipsis-vertical-C7kfeCs2.js
@@ -0,0 +1,6 @@
+import{M as c}from"./createLucideIcon-CVZX51G5.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}],["circle",{cx:"12",cy:"5",r:"1",key:"gxeob9"}],["circle",{cx:"12",cy:"19",r:"1",key:"lyex9k"}]],o=c("ellipsis-vertical",e);export{e as __iconNode,o as default};
