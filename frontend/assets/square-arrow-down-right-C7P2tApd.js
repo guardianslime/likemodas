1f6790c305fd2984d6c68167e9c948d22df25e58
guardianslime <k1navia0@gmail.com>
2025-07-25 19:38:49 -0500

    Initial Commit

--- frontend/assets/square-arrow-down-right-C7P2tApd.js
+++
@@ -1,6 +0,0 @@
-import{M as t}from"./createLucideIcon-CVZX51G5.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
- * @license lucide-react v0.511.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}],["path",{d:"m8 8 8 8",key:"1imecy"}],["path",{d:"M16 8v8H8",key:"1lbpgo"}]],a=t("square-arrow-down-right",o);export{o as __iconNode,a as default};
