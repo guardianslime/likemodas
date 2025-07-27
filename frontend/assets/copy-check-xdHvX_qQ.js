eb5beab22b5a2f7c79e17cd421db2ef2287ad91e
guardianslime <k1navia0@gmail.com>
2025-07-27 16:36:38 -0500

    Initial Commit

---
+++ frontend/assets/copy-check-xdHvX_qQ.js
@@ -0,0 +1,6 @@
+import{L as c}from"./createLucideIcon-Cw7lnmM3.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"m12 15 2 2 4-4",key:"2c609p"}],["rect",{width:"14",height:"14",x:"8",y:"8",rx:"2",ry:"2",key:"17jyea"}],["path",{d:"M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2",key:"zix9uf"}]],r=c("copy-check",e);export{e as __iconNode,r as default};
