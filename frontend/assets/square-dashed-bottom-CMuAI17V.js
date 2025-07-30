3e3f4f1a9fc80d489e5e2ed9c0c6ac8b29c83009
guardianslime <k1navia0@gmail.com>
2025-07-30 02:23:10 -0500

    Initial Commit

---
+++ frontend/assets/square-dashed-bottom-CMuAI17V.js
@@ -0,0 +1,6 @@
+import{G as a}from"./createLucideIcon-B1I6BdvF.js";import"./chunk-QMGIS6GS-suYYFPSk.js";import"./index-GZitOFEf.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M5 21a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2",key:"as5y1o"}],["path",{d:"M9 21h1",key:"15o7lz"}],["path",{d:"M14 21h1",key:"v9vybs"}]],s=a("square-dashed-bottom",o);export{o as __iconNode,s as default};
