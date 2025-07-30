c3f84af77bbd4e0935764f7635ecd3aafc603ee8
guardianslime <k1navia0@gmail.com>
2025-07-30 01:52:35 -0500

    Initial Commit

---
+++ frontend/assets/square-x-Ct1pKTsn.js
@@ -0,0 +1,6 @@
+import{G as e}from"./createLucideIcon-B1I6BdvF.js";import"./chunk-QMGIS6GS-suYYFPSk.js";import"./index-GZitOFEf.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",ry:"2",key:"1m3agn"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]],i=e("square-x",t);export{t as __iconNode,i as default};
