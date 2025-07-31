974ec7057f5538b2e33607b706b1d86e3a5dfe48
guardianslime <k1navia0@gmail.com>
2025-07-31 16:21:44 -0500

    Initial COmmit

--- likemodas/models/__init__.py
+++ likemodas/models/__init__.py
@@ -1,6 +1,8 @@
 from .user import UserInfo
 from .auth import LocalUser
 from .token import VerificationToken, PasswordResetToken
+# --- CAMBIO: Se añade la importación y exportación de ProductCardData ---
+from .product_data import ProductCardData
 from .blog import BlogPostModel
 from .cart import PurchaseModel, PurchaseItemModel
 from .shipping import ShippingAddressModel
@@ -10,6 +12,7 @@ from .enums import UserRole, Category, PurchaseStatus, VoteType
 
 __all__ = [
     "UserInfo", "LocalUser", "VerificationToken", "PasswordResetToken",
+    "ProductCardData", # <-- Se añade a la lista __all__
     "BlogPostModel", "PurchaseModel", "PurchaseItemModel", "ShippingAddressModel",
     "ContactEntryModel", "NotificationModel", "CommentModel", "CommentVoteModel",
     "UserRole", "Category", "PurchaseStatus", "VoteType"
