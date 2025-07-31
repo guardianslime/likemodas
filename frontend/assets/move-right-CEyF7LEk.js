5cefc65838dc58874f5a2543497023540dd3a894
guardianslime <k1navia0@gmail.com>
2025-07-31 01:32:26 -0500

    Initial Commit

---
+++ frontend/assets/move-right-CEyF7LEk.js
@@ -0,0 +1,6 @@
+import{L as o}from"./createLucideIcon-BaKMIaGk.js";import"./chunk-QMGIS6GS-CgUBpFMi.js";import"./index-B9RKNTce.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M18 8L22 12L18 16",key:"1r0oui"}],["path",{d:"M2 12H22",key:"1m8cig"}]],c=o("move-right",t);export{t as __iconNode,c as default};
