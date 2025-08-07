d3402b3f4240559feb7499bd7b713b48bf854370
guardianslime <k1navia0@gmail.com>
2025-08-06 23:29:21 -0500

    Initial Commit

---
+++ frontend/assets/stateful_components-CDTXBJln.js
@@ -0,0 +1,6 @@
+import{a as _,v as e}from"./chunk-QMGIS6GS-Bap335uN.js";import{h as s,j as t}from"./state-B4ezycYv.js";import{K as r,D as o,F as c,H as i}from"./createLucideIcon-D10Um5ce.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["circle",{cx:"8",cy:"21",r:"1",key:"jimo8o"}],["circle",{cx:"19",cy:"21",r:"1",key:"13723u"}],["path",{d:"M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12",key:"9zh506"}]],p=r("shopping-cart",n);function d(){const a=_.useContext(s.reflex___state____state__likemodas___state____app_state);return t(i,{asChild:!0,css:{"&:hover":{color:"var(--accent-8)"}}},t(e,{to:"/cart"},t(o,{align:"start",className:"rx-Stack",direction:"row",gap:"3"},t(p,{}),t(c,{color:"red",variant:"solid"},a.cart_items_count_rx_state_))))}export{d as L};
