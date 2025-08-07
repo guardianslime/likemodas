7abac5f83908718dffed22a4502edaad5b175a96
guardianslime <k1navia0@gmail.com>
2025-08-06 22:55:52 -0500

    Initial Commit

--- likemodas/components/base.py
+++ likemodas/components/base.py
@@ -1,14 +1,14 @@
-# likemodas/components/base.py (CORREGIDO)
+# likemodas/components/base.py (CORREGIDO Y DEFINITIVO)
 
 import reflex as rx
 from .navbar import navbar
+from reflex.components.sonner import Toaster # <--- 1. AÑADE ESTA IMPORTACIÓN DIRECTA
 
 def base_page(child: rx.Component) -> rx.Component:
     """Un layout base que incluye la navbar y el contenedor de notificaciones."""
     return rx.box(
         navbar(),
         child,
-        # ▼▼▼ ESTA ES LA LÍNEA CORREGIDA Y DEFINITIVA ▼▼▼
-        # El alias correcto es rx.toaster (todo en minúscula)
-        rx.toaster(position="bottom-right", theme="dark"),
+        # ▼▼▼ 2. USA EL COMPONENTE IMPORTADO DIRECTAMENTE ▼▼▼
+        Toaster(position="bottom-right", theme="dark"),
     )
