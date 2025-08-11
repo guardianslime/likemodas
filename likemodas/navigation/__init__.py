caa4829bd7f0991c264b74b126b045f7c10a7069
guardianslime <k1navia0@gmail.com>
2025-08-11 16:04:22 -0500

    Initial Commit

--- likemodas/navigation/__init__.py
+++
@@ -1,9 +0,0 @@
-# likemodas/navigation/__init__.py (CORREGIDO)
-
-from . import routes
-
-# Ya no se exporta NavState porque fue fusionado en AppState.
-# Los métodos de navegación se llaman directamente desde AppState.
-__all__ = [
-    "routes",
-]
