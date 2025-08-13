bbef4fa8ed85ad9cb8178984f0f6d894b5a8ea80
guardianslime <k1navia0@gmail.com>
2025-08-13 00:49:42 -0500

    Initial  Commit

--- likemodas/likemodas.py
+++ likemodas/likemodas.py
@@ -20,7 +20,7 @@ from .account import shipping_info as shipping_info_module
 # ✨ CORRECCIÓN FINAL: Importaciones que coinciden con tus nombres de archivo ✨
 from .blog.public_page import blog_public_page_content
 from .blog.detail import blog_post_detail_content
-from .blog.admin_page import blog_post_list_content
+from .blog.list import blog_post_list_content
 from .blog.add import blog_post_add_content
 
 
