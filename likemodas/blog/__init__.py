48e44b8d8a01cd010e27705c6da22212b6b8147c
guardianslime <k1navia0@gmail.com>
2025-08-13 00:42:53 -0500

    Initial COmmit

--- likemodas/blog/__init__.py
+++ likemodas/blog/__init__.py
@@ -5,7 +5,7 @@ from .add import blog_post_add_content
 from .detail import blog_post_detail_content
 from .list import blog_post_list_content
 from .public_page import blog_public_page_content
-from .detail import blog_public_detail_content
+from .detail import blog_post_detail_content
 from .admin_page import blog_admin_page
 
 # __all__ le dice a Python qué funciones están disponibles desde el paquete 'blog'
@@ -14,6 +14,6 @@ __all__ = [
     'blog_post_detail_content',
     'blog_post_list_content',
     'blog_public_page_content',
-    'blog_public_detail_content',
+    'blog_post_detail_content',
     'blog_admin_page',
 ]
