5a242f80470716cbbb63a36b7f7041d9969ce9dd
guardianslime <k1navia0@gmail.com>
2025-08-13 00:40:43 -0500

    Initial COMMIT

--- likemodas/blog/__init__.py
+++ likemodas/blog/__init__.py
@@ -2,17 +2,18 @@
 
 # Importa cada función desde su archivo de origen correcto
 from .add import blog_post_add_content
-from .admin_page import blog_admin_page, blog_post_list_content, blog_post_detail_content, blog_post_edit_content
+from .detail import blog_post_detail_content
+from .list import blog_post_list_content
 from .public_page import blog_public_page_content
 from .detail import blog_public_detail_content
+from .admin_page import blog_admin_page
 
 # __all__ le dice a Python qué funciones están disponibles desde el paquete 'blog'
 __all__ = [
     'blog_post_add_content',
-    'blog_admin_page',
-    'blog_post_list_content',
     'blog_post_detail_content',
-    'blog_post_edit_content',
+    'blog_post_list_content',
     'blog_public_page_content',
     'blog_public_detail_content',
+    'blog_admin_page',
 ]
