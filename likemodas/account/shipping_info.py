1e608fbf0ba0aeaaac5050d08d788f92d61a9bf4
guardianslime <k1navia0@gmail.com>
2025-08-22 20:51:17 -0500

    Initial Commit

--- likemodas/account/shipping_info.py
+++ likemodas/account/shipping_info.py
@@ -90,7 +90,7 @@ def address_card(address: ShippingAddressModel) -> rx.Component:
                 address.latitude,
                 rx.link(
                     rx.badge(
-                        rx.icon(tag="check-circle", size=14), # <-- ASÍ QUEDA CORREGIDO
+                        rx.icon(tag="check-check", size=14), # <-- ✨ SOLUCIÓN FINAL ✨
                         "Ubicación guardada. Ver en mapa.",
                         color_scheme="blue",
                         variant="soft",
