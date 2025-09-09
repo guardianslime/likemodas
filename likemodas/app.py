d81336b409a8c575bc924de35e786f9a03da9054
guardianslime <k1navia0@gmail.com>
2025-09-09 18:55:45 -0500

    Initial Commit

--- likemodas/app.py
+++
@@ -1,5 +0,0 @@
-# likemodas/app.py
-import reflex as rx
-
-# Define la instancia de la aplicación aquí para que pueda ser importada en otros lugares.
-app = rx.App(style={"font_family": "Arial, sans-serif"})
