00ae2bc6359974a9d09a211ce324dda56bc18485
guardianslime <k1navia0@gmail.com>
2025-09-09 16:46:49 -0500

    Initia lCommit

--- likemodas/pages/payment_pending.py
+++ likemodas/pages/payment_pending.py
@@ -5,7 +5,7 @@ def payment_pending_page() -> rx.Component:
     """Página que se muestra después de finalizar una compra online."""
     return rx.center(
         rx.vstack(
-            rx.icon(tag="check_circle", size=64, color_scheme="green"),
+            rx.icon(tag="check_check", size=64, color_scheme="green"),
             rx.heading("¡Tu pedido ha sido recibido!", size="7"),
             rx.text(
                 "Gracias por tu compra. El vendedor ha sido notificado y te enviará el enlace de pago en breve.",
