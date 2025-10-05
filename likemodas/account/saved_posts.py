a9ddd922e69c64a25ddd488505dbc40309e8dc6d
guardianslime <k1navia0@gmail.com>
2025-10-05 17:18:28 -0500

    Initial Commit

--- likemodas/account/saved_posts.py
+++ likemodas/account/saved_posts.py
@@ -9,9 +9,8 @@ from ..ui.skeletons import skeleton_product_gallery
 from ..blog.public_page import product_detail_modal
 
 @reflex_local_auth.require_login
-def saved_posts_page() -> rx.Component:
+def saved_posts_content() -> rx.Component: # <-- NOMBRE CORREGIDO
     """Página que muestra las publicaciones guardadas por el usuario."""
-    # --- INICIO DE LA CORRECCIÓN DE LAYOUT ---
     page_content = rx.vstack(
         rx.heading("Mis Publicaciones Guardadas", size="8", text_align="center"),
         rx.text("Aquí encontrarás los productos que has guardado para ver más tarde.", margin_bottom="1.5em", color_scheme="gray", size="4", text_align="center"),
@@ -34,9 +33,8 @@ def saved_posts_page() -> rx.Component:
         ),
         align="center",
         width="100%",
-        max_width="1600px" # Ancho máximo para la galería
+        max_width="1600px"
     )
-    # --- FIN DE LA CORRECCIÓN DE LAYOUT ---
     
     return rx.fragment(
         account_layout(page_content),
