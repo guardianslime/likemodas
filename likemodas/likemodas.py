389f792d54e49c60a4d8c0612b19e728e7d45ffb
guardianslime <k1navia0@gmail.com>
2025-10-18 10:51:22 -0500

    Iniitla Commit

--- likemodas/likemodas.py
+++ likemodas/likemodas.py
@@ -30,7 +30,8 @@ from .admin.users_page import user_management_page
 from .admin import employees_page # Importa la nueva página
 
 # Vistas de Blog y Productos
-from .blog import blog_admin_page, blog_post_add_content
+from .blog.admin_page import blog_admin_page
+from .blog.add import blog_post_add_content
 # Se importa directamente desde el archivo específico para evitar el ciclo.
 from .blog.admin_page import blog_admin_page
 from .blog.add import blog_post_add_content
