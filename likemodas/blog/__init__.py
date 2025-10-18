20fb042366b4b798eef611fed7099ea82c1898c7
guardianslime <k1navia0@gmail.com>
2025-10-18 11:38:11 -0500

    Initial Commit

--- likemodas/blog/__init__.py
+++ likemodas/blog/__init__.py
@@ -1,5 +1,16 @@
-# En: likemodas/blog/__init__.py
+# likemodas/blog/__init__.py (SIN CAMBIOS)
 
-# Este archivo ahora puede estar casi vacío.
-# Su única función es marcar el directorio 'blog' como un paquete de Python.
-# Ya no necesitamos que re-exporte el contenido de otros archivos.
+from .add import blog_post_add_content
+from .detail import blog_post_detail_content
+from .list import blog_post_list_content
+from .public_page import blog_public_page_content
+from .admin_page import blog_admin_page
+
+
+__all__ = [
+    'blog_post_add_content',
+    'blog_post_detail_content',
+    'blog_post_list_content',
+    "blog_public_page_content",
+    "blog_admin_page",
+]
