a668b8d2957a62aab2e211d48d1216fd3c71aa49
guardianslime <k1navia0@gmail.com>
2025-11-11 13:06:42 -0500

    Initail Commit

--- likemodas/account/__init__.py
+++ likemodas/account/__init__.py
@@ -1,16 +1,13 @@
-# likemodas/account/__init__.py (Versión Corregida)
+# likemodas/account/__init__.py (Esta versión es la correcta)
 
 from . import page
 from . import shipping_info
 from .layout import account_layout
 from .sidebar import account_sidebar
-# from . import display_settings_page # <--- ELIMINA ESTA LÍNEA
 
-# Se eliminan las exportaciones de los estados antiguos.
 __all__ = [
     "page", 
     "shipping_info", 
     "account_layout", 
     "account_sidebar",
-    # "display_settings_page", # <--- ELIMINA ESTA LÍNEA
 ]
